9105fc2766a6a20da0537dbf745d00fd4a1559b5
unknown <jonathan.n.haedo@gmail.com>
2021-11-04 13:56:58 -0300

    primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import {AngularFireModule} from '@angular/fire/compat';
+import { HomeComponent } from './components/home/home.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
