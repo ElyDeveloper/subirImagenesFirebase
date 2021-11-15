987ab967df9971c3465f61ee3f07d6b845d0ff3f
unknown <jonathan.n.haedo@gmail.com>
2021-11-14 22:08:43 -0300

    codigo correcto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { StorageService } from './services/storage.service';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(private storageService: StorageService) {
+
+  }
+
+  imagenes: any[] = [];
+  cargarImagen(event: any) {
+    let archivos = event.target.files;
+    let nombre = "jonathan";
+
+    for (let i = 0; i < archivos.length; i++) {
+
+      let reader = new FileReader();
+      reader.readAsDataURL(archivos[0]);
+      reader.onloadend = () => {
+        console.log(reader.result);
+        this.imagenes.push(reader.result);
+        this.storageService.subirImagen(nombre + "_" + Date.now(), reader.result).then(urlImagen => {
+          let usuario = {
+            name: "jonathan",
+            nickName: "yonykikok",
+            password: "401325",
+            imgProfile: urlImagen
+          }
+          console.log(urlImagen);
+        });
+      }
+    }
+
+
+
+
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAxP0rUahxCXjtf7qBEadUXfr3WvWk1alU",
+    authDomain: "tp-ppsii.firebaseapp.com",
+    databaseURL: "https://tp-ppsii-default-rtdb.firebaseio.com",
+    projectId: "tp-ppsii",
+    storageBucket: "tp-ppsii.appspot.com",
+    messagingSenderId: "756468356457",
+    appId: "1:756468356457:web:c12be35b2de3dfff1619f8",
+    measurementId: "G-3827X50ZTP"
+  }
 };
 
 /*
